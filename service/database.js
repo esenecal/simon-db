586299c7e3048c194172a2241650ab2288f9c48d
esenecal <55216700+esenecal@users.noreply.github.com>
2025-11-10 10:09:11 -0700

    Added a line of code to clear the click collection at the beginning of the server run.

--- service/database.js
+++ service/database.js
@@ -13,6 +13,8 @@ const clickCollection = db.collection('clicks');    // Added a collection for ho
   try {
     await db.command({ ping: 1 });
     console.log(`Connect to database`);
+    // Clear the clickCollection collection.
+    clickCollection.deleteMany();
   } catch (ex) {
     console.log(`Unable to connect to database with ${url} because ${ex.message}`);
     process.exit(1);
